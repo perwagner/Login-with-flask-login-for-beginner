950752c4fd947ed0b503b10a9d1cbc070d83d316
Per Wagner Nielsen <perwagnernielsen@gmail.com>
2017-11-26 08:09:58 +0800

    updated

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Email, DataRequired
 
-class SignupForm(Form):
+class SignupForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField("Sign In")
